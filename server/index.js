9a521c5820ff3bb8142bbcf0bf3f4ffb4c813ec2
Ishan Vyas <ishan.official01@gmail.com>
2021-12-15 16:49:00 -0700

    Database initalisation

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
-app.get(
-    "/", (req,res) => {
-        res.send("hello world");
-    }
-);
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'medical_workflow'
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+app.get("/", (req,res) => {
+    const sqlInsert = "INSERT INTO disease (name) VALUES ('infection');";
+    const sqlDelete = "DELETE FROM disease WHERE dsid = 3;";
+    db.query(sqlDelete, (err, result) => {
+        if (err) throw err;
+        console.log("Result: " + result);
+        res.send("hello world this is my name jhjh");
+    });
+});
 
 app.listen(
     3001, () => {
